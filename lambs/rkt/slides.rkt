cd43beb390df2ee75d43062015b091a397643514
Jonas Winje <jonas.winje@gmail.com>
2017-04-21 19:21:48 +0200

    test-slide

--- lambs/rkt/slides.rkt
+++ lambs/rkt/slides.rkt
@@ -1,12 +1,24 @@
 #lang racket
 (provide slides)
 
-(require "rules.rkt")
+(require "rules.rkt"
+         "draw-proof.rkt"
+         "infer-structs.rkt"
+         "structs.rkt")
+
+(define test-slide
+  (draw-proof-typey
+   (inference (coloured (sequent (list (: (ref '_) (ty 'TEST))) (: (ref '_) (ty '|testity test test|))) 'blue)
+              (rule '? #f)
+              (list (inference (coloured (sequent '() (: (ref '_) (ty 'MOOP))) 'green)
+                               (rule '? #f)
+                               (list (coloured (sequent (list (: (ref '_) (ty '|MEEEP|))) (: (ref '_) (ty '|squee|))) 'black)))))))
 
 (define slides
   (make-immutable-hash
    `((structural-rules . ,(list (draw-rules struct-rules)))
      (function-rules . ,(list (draw-rules fun-rules)))
      (product-rules . ,(list (draw-rules prod-rules)))
-     (sum-rules . ,(list (draw-rules sum-rules))))))
+     (sum-rules . ,(list (draw-rules sum-rules)))
+     (test . ,(list test-slide)))))
 
